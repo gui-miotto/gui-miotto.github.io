566777cac6e6e9cd72e5b9a60bd424744e10a760
Gui Miotto <guilherme.miotto@gmail.com>
2020-06-13 23:14:56 +0200

    Add particles.js

---
+++ script/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('sec_contact', 'script/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
