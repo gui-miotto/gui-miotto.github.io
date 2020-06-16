0ea1129fe0790111baa38c5ba41d640d23dcf949
Gui Miotto <guilherme.miotto@gmail.com>
2020-06-17 00:39:55 +0200

    Converts to Jekyll

--- assets/js/particles_app.js
+++ assets/js/particles_app.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('sec_contact', 'script/particles.json', function() {
+particlesJS.load('sec_contact', 'assets/js/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
--- script/index.js
+++
@@ -1,4 +0,0 @@
-let year = "2023";
-
-document.write("Boots on the moon by " + year);
-console.log(year)
